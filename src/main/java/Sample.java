16fa58992c8ad6442f7e094ac00faa3abcbdbfdb
Muga Nishizawa <muga.nishizawa@gmail.com>
2015-09-29 15:34:16 -0700

    added encryption keys in .travis.yml

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -3,6 +3,9 @@ public class Sample
     public void doIt()
     {
         System.out.println("call doIt method");
+        System.out.println("VAR1: " + System.getenv("VAR1"));
+        System.out.println("VAR2: " + System.getenv("VAR2"));
+        System.out.println("VAR3: " + System.getenv("VAR3"));
     }
 
     public static void main(String[] args)
