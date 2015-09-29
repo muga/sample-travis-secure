c937b9408fdb7c9ca6b1c8e395d787231b3e6e3d
Muga Nishizawa <muga.nishizawa@gmail.com>
2015-09-29 15:08:21 -0700

    add sample programs

---
+++ src/main/java/Sample.java
@@ -0,0 +1,12 @@
+public class Sample
+{
+    public void doIt()
+    {
+        System.out.println("call doIt method");
+    }
+
+    public static void main(String[] args)
+    {
+        new Sample().doIt();
+    }
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TestSample
+{
+    @Test
+    public void doIt()
+    {
+        new Sample().doIt();
+    }
+}
